9b533f18df7aca87cba8df2e756c57d3f5dafb7e
felipe <felipepcandido8@gmail.com>
2025-03-09 19:29:48 -0300

    Configurei o app de cadastro e o banco de dados para posteriormente criar APIs para registro e login de usuários

--- backend/projeto/cadastro/views.py
+++ backend/projeto/cadastro/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
 
 # Create your views here.
 
 def home(request):
     return render(request, 'home/home.html', {'user': request.user})
+
+@api_view
+def apiCadastro(request):
+    return
