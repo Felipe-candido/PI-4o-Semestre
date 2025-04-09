6d1a505df1cb276414e289af49eda482e0f46763
Felipe-candido <felipepcandido8@gmail.com>
2025-04-08 22:36:48 -0300

    adicionando segunda versao do frontend

--- backend/projeto/cadastro/views.py
+++ backend/projeto/cadastro/views.py
@@ -123,12 +123,12 @@ class viewLogout(viewsets.ViewSet):
 class CookieJWTAuthentication(JWTAuthentication):
     def authenticate(self, request):
         # Tenta pegar o token do cookie
-        raw_token = request.COOKIES.get('access_token')
+        token = request.COOKIES.get('access_token')
         
-        if raw_token is None:
+        if token is None:
             return None
             
-        validated_token = self.get_validated_token(raw_token)
+        validated_token = self.get_validated_token(token)
         return self.get_user(validated_token), validated_token
 
 class UserProfileView(viewsets.ViewSet):
