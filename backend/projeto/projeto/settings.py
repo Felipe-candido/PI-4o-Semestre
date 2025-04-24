110e537ba23445b6883859cc1a4e1bcdad217d91
Marquinho Amorim <67428125+MarquinhoAmorim@users.noreply.github.com>
2025-04-23 23:42:58 -0300

    Campos Editaveis | Profile Tenant

--- backend/projeto/projeto/settings.py
+++ backend/projeto/projeto/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'app_chacaras',
         'USER': 'postgres',
-        'PASSWORD': '123',
+        'PASSWORD': 'postgres',
         'HOST': 'localhost',
         'PORT': '5432',
     }
