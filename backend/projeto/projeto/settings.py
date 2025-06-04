66bcff748167862a6c2d681dbeb42c880e9ac536
felipe <felipepcandido8@gmail.com>
2025-06-04 20:51:04 -0300

    ajustando git ignore e variaveis de ambiente

--- backend/projeto/projeto/settings.py
+++ backend/projeto/projeto/settings.py
@@ -183,7 +183,7 @@ AUTH_USER_MODEL = 'cadastro.usuario'
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 # Carrega o .env da raiz
-load_dotenv(BASE_DIR / '.env')
+load_dotenv(BASE_DIR / 'tokens.env')
 
 GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
 GOOGLE_CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET")
